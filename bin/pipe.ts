7faad2db81da76140a92a327923c4e73f26129a6
Kaizen Conroy <kcc2@williams.edu>
2020-08-18 16:01:15 -0400

    pipe stack

--- bin/pipe.ts
+++ bin/pipe.ts
@@ -1,6 +1,40 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
+import { Construct, SecretValue, Stage, Stack, StackProps, StageProps, App } from '@aws-cdk/core';
 import { PipeStack } from '../lib/pipe-stack';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
-const app = new cdk.App();
-new PipeStack(app, 'PipeStack');
+
+class MyPipelinestack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, "Pipeline", {
+      pipelineName: 'TestPipeline',
+      cloudAssemblyArtifact,
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: "GitHub",
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('GithubToken'),
+        owner: "kaizen3031593",
+        repo: "pipe-pump",
+      }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+      })
+    });
+  }
+}
+
+const app = new App();
+new MyPipelinestack(app, 'PipelineStack', {
+  env: {
+    account: '489318732371',
+    region: 'us-east-1',
+  }
+});
